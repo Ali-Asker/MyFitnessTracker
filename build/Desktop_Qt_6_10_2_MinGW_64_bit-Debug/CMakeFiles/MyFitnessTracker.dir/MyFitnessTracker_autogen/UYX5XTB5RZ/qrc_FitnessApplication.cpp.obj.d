48721de7abe7ed114a3480329de0067ba388dbe8
Joshua MacPherson <macp0161@algonquinlive.com>
2026-03-17 16:20:36 -0400

    changed gitignore

--- build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/CMakeFiles/MyFitnessTracker.dir/MyFitnessTracker_autogen/UYX5XTB5RZ/qrc_FitnessApplication.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/MyFitnessTracker.dir/MyFitnessTracker_autogen/UYX5XTB5RZ/qrc_FitnessApplication.cpp.obj: \
- C:\Users\josh1\OneDrive\Documents\Semester\ 4\C++\MyFitnessTracker\build\Desktop_Qt_6_10_2_MinGW_64_bit-Debug\MyFitnessTracker_autogen\UYX5XTB5RZ\qrc_FitnessApplication.cpp
