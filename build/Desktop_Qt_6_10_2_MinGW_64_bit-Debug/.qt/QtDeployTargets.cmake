ee4b8e722ec1a255cb6a1cabace1507507d73d10
josh <93330898+joshuamacpherson@users.noreply.github.com>
2026-03-17 19:43:40 -0400

    Delete build directory
    phinees branch

--- build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_MyFitnessTracker_FILE C:/C++/MyFitnessTracker/MyFitnessTracker/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/MyFitnessTracker.exe)
-set(__QT_DEPLOY_TARGET_MyFitnessTracker_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_MyFitnessTracker_RUNTIME_DLLS C:/Qt/6.10.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
